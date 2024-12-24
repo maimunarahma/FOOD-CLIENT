fe14cfa3c669f43e6939e26029b96a9f2f0255e1
Maimuna Rahman <maimunarahman30@gmail.com>
2024-12-25 02:38:15 +0600

    food deleted from a colllection and inserted to a new collection

--- src/Pages/Home/Details.jsx
+++ src/Pages/Home/Details.jsx
@@ -1,15 +1,22 @@
 import { useLoaderData } from "react-router-dom";
-
+import { Link } from "react-router-dom";
  
  const Details = () => {
     const data=useLoaderData();
     console.log(data)
     
+    const handleRequest=()=>{
+        fetch(`http://localhost:4000/transfer/${data._id}`)
+        .then(res=> res.json())
+        .then(data=>{console.log(data)
+            alert('Food item successfully requested!');
+        })
+    }
     return (
         <div key={data._id} >
       <img src={data.foodImg} alt="" />
-                <h1 className="font-bold text-6xl">{data.foodName}</h1>
-          
+                <h1 className="font-bold text-6xl" >{data.foodName}</h1>
+           <button onClick={handleRequest} className="btn">Request food</button>
         </div>
     );
  };
